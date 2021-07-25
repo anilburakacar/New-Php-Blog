e64bd07295a2c6f3dad2ab85cf2a7603c2709371
anilburakacar <anilacar_1999@hotmail.com>
2021-07-25 19:00:18 +0300

    test

--- myblog/admin/controller/categories.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$query = $db->prepare('SELECT * FROM categories order by category_order asc ');
-$query->execute();
-$query = $query->fetchAll(PDO::FETCH_ASSOC);
-require admin_view('categories');
--- myblog/admin/controller/contact.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$query = $db->prepare('SELECT * FROM contact order by contact_id desc ');
-$query->execute();
-$query = $query->fetchAll(PDO::FETCH_ASSOC);
-require admin_view('contact');
